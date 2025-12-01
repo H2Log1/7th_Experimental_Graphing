c50c97480257c7547d6b7a9632acfcb2ec5d39e7
H2log1 <xyychat996@hotmail.com>
2025-12-01 20:11:42 +0800

    4th save

--- plain_bearing_1.py
+++ plain_bearing_1.py
@@ -14,7 +14,7 @@ B = 125
 f1 = np.array([1.24,0.01,0.03,0.02,0.01,0.02,0.01,0.01,5.76])
 n1 = np.array([350, 250, 150, 100, 50, 30, 20, 10, 5])
 
-f2 = np.array([0.52,0.02,0.01,0.02,0.01,0.02,0.01,0.04,0.01])
+f2 = np.array([0.52,0.02,0.01,0.02,0.01,0.02,0.01,0.01,5.47])
 n2 = np.array([350, 250, 150, 100, 50, 30, 20, 10, 5])
 
 # f3 = np.array([0.02,0.01,0.02,0.01])
@@ -23,10 +23,10 @@ n2 = np.array([350, 250, 150, 100, 50, 30, 20, 10, 5])
 # f4 = np.array([0.01,0.03,0.02,0.01])
 # n4 = np.array([250, 150, 100, 50])
 
-f = np.flip(f1)
-n = np.flip(n1)
+f = np.flip(f2)
+n = np.flip(n2)
 
-p = W1 / (d * B)
+p = W2 / (d * B)
 λ = n * η / p
 
 x = λ
@@ -41,7 +41,7 @@ print(y)
 plt.plot(X_, Y_, linestyle='-')
 plt.xlabel('λ')
 plt.ylabel('f')
-plt.title('滑动系数与滑动率关系曲线（600N） （Drawn by HZQ）')
+plt.title('滑动系数与滑动率关系曲线（400N） （Drawn by HZQ）')
 plt.grid(True)
-# plt.savefig(r"output\\plain_bearing_f_600N.png", dpi=300)
+plt.savefig(r"output\\plain_bearing_f_400N.png", dpi=300)
 plt.show()
